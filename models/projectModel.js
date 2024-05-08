492e52993190bb7d1503004bacfa97b743ca1346
Shaik Sajid Hussain <194G1A0291@Srit.ac.in>
2024-05-08 15:21:37 +0530

    Updated the date in table

--- models/projectModel.js
+++ models/projectModel.js
@@ -23,7 +23,16 @@ const projectSchema=new mongoose.Schema({
     message:{
         type:String,
         required:true
+    },
+    date: {
+		type: Date,
+		default: () => {
+			const ISTOffset = 330 * 60 * 1000; // Offset in milliseconds for IST (UTC+5:30)
+			const dateIST = new Date(Date.now() + ISTOffset);
+			return dateIST;
+		},
     }
+
 })
 const Project=mongoose.model('Project',projectSchema)
 module.exports=Project
